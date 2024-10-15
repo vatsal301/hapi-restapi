ba2d48f397082140bb233066b09c657f65b3e0d2
vatsal301 <vatsalgajjar8623@gmail.com>
2024-10-15 11:10:08 +0530

    gitignore config

---
+++ server.js
@@ -0,0 +1,26 @@
+const Hapi = require("@hapi/hapi");
+const logger = require("./logger");
+
+const init = async () => {
+  const server = Hapi.server({
+    host: "localhost",
+    port: 3000,
+  });
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "hello";
+    },
+  });
+
+  await server.start();
+  logger.info(`Server is Started at ${server.info.uri}`);
+};
+init();
+
+process.on("uncaughtException", (error) => {
+  logger.error(error);
+  process.exit(1);
+});
